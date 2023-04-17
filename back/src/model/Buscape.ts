260cad839d671e6dbc30c685e72621f583665b53
JackCaio <caio.iack1@gmail.com>
2023-04-17 14:25:05 -0300

    Fix buscape products with missing images

--- back/src/model/Buscape.ts
+++ back/src/model/Buscape.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import { load } from 'cheerio';
 import { IResult } from './MercadoLivre';
 // https://www.buscape.com.br/search?q=brastemp&hitsPerPage=48&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D=8&page=1&sortBy=default&isDealsPage=false&enableRefinementsSuggestions=true
 // https://www.buscape.com.br/search?q=Brastemp&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D=8  Geladeira
@@ -7,6 +7,8 @@ import { IResult } from './MercadoLivre';
 // https://www.buscape.com.br/search?q=Samsung&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D=7  Celular
 
 // https://www.buscape.com.br/search?q=&hitsPerPage=48&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D=8&page=2
+
+const base64Regex = /^data:/
 class Buscape {
   private static baseUrl = 'https://www.buscape.com.br/search?q=';
   private static categoryUrl = '&hitsPerPage=48&refinements%5B0%5D%5Bid%5D=categoryId&refinements%5B0%5D%5Bvalues%5D%5B0%5D='
@@ -18,7 +20,16 @@ class Buscape {
     const categoryCode = this.categorySelect(category);
     const siteUrl = `${this.baseUrl}${query || ''}${this.categoryUrl}${categoryCode}`;
     const redirUrl = `https://www.buscape.com.br/${category.toLowerCase()}`;
-    const result = await this.productScrape(siteUrl, redirUrl);
+    const searchRes = await this.productScrape(siteUrl, redirUrl);
+
+    const photoSolve = searchRes.map(async (element) => {
+      const photo = base64Regex.test(element.photo) ?
+        await this.fixImageSrc(element.permalink) : element.photo;
+      return { ...element, photo };
+    });
+
+    const result = await Promise.all(photoSolve);
+
     return result.map((product, index) => ({id: `${index}`, category, ...product}))
   }
 
@@ -36,22 +47,31 @@ class Buscape {
   }
 
   private static async productScrape(url: string, redirectUrl: string) {
-    return axios.get(url)
-      .then(({ data }) => {
-        const doc = cheerio.load(data);
+    return axios.get(url).then(({ data }) => {
+      const doc = load(data);
 
-        return doc('.SearchCard_ProductCard_Inner__7JhKb')
-          .map((_, element) => {
-            const description = doc(element)
-              .find('.SearchCard_ProductCard_Name__ZaO5o').text();
-            const photo = doc(element).find('img').attr('src');
-            const price = doc(element)
-              .find('p.Text_Text__h_AF6.Text_MobileHeadingS__Zxam2').text();
-            const permalink = `${redirectUrl}${doc(element).attr('href')}`;
+      return doc('.SearchCard_ProductCard_Inner__7JhKb')
+        .map((_, element) => {
+          const description = doc(element)
+            .find('.SearchCard_ProductCard_Name__ZaO5o').text();
+          const photo = doc(element).find('img').attr('src') || '';
+          const price = doc(element)
+            .find('p.Text_Text__h_AF6.Text_MobileHeadingS__Zxam2').text();
+          const permalink = `${redirectUrl}${doc(element).attr('href')}`;
 
-            return {description, photo: photo || '', price, permalink}
-          }).toArray();
-      });
+          return { description, photo, price, permalink }
+        }).toArray();
+    });
+  }
+
+  private static async fixImageSrc(url: string) {
+    return await axios.get(url).then(({ data }) => {
+      const doc = load(data);
+
+      const product = doc('img.swiper-lazy').attr('data-src') || '';
+
+      return product
+    })
   }
 }
 
